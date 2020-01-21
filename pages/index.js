5bc5cb58a02d3bc789e664c9a13662195bbcfc85
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-21 19:01:30 +0800

    ➕ Imported <RecentProjects />.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from 'components/Layout/layout';
 import Hero from 'components/Hero/hero';
+import RecentProjects from 'components/RecentProjects/recentProjects';
 
 const Home = () => (
   <Layout title="Home">
     <Hero />
+    <RecentProjects />
   </Layout>
 );
 
