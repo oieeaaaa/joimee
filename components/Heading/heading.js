0210c34320f75943052368d4d7056524d11749ff
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-23 21:02:04 +0800

    🎨 Enable heading to accept props.

--- components/Heading/heading.js
+++ components/Heading/heading.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Heading = ({ text }) => <h3 className="heading">{text}</h3>;
+const Heading = ({ className, text, ...rest }) => (
+  <h3
+    className={className ? `${className} heading` : 'heading'}
+    {...rest}
+  >
+    {text}
+  </h3>
+);
+
+Heading.defaultProps = {
+  className: '',
+};
 
 Heading.propTypes = {
   text: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export default Heading;
