1db2760b584d0623f78c487d255293b091397219
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-29 19:41:31 +0800

    🔥 Removed dummy data.

--- components/Card/card.js
+++ components/Card/card.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// to be removed after development.
-export const FAKE_PROJECTS = [
-  {
-    title: 'Co Hiking',
-    path: 'co-hiking',
-    imgLink: 'https://bit.ly/2NJbGXu',
-    color: '#D6E5DC',
-  },
-  {
-    title: 'Sentinental Software',
-    path: 'sentinental-software',
-    imgLink: 'https://bit.ly/2sJtFWJ',
-    color: '#358F6C',
-  },
-  {
-    title: 'Vespa',
-    path: 'vespa',
-    imgLink: 'https://cdn.dribbble.com/users/4189231/screenshots/9653758/media/634bcc1a23220d074f322749524f423e.jpg',
-    color: '#FACB05',
-  },
-];
-
 const Card = ({ title, imgLink, color }) => (
   <div className="card">
     <figure className="card__img-container">
@@ -45,10 +23,14 @@ const Card = ({ title, imgLink, color }) => (
   </div>
 );
 
+Card.defaultProps = {
+  color: '#e2e2e2',
+};
+
 Card.propTypes = {
   title: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
   imgLink: PropTypes.string.isRequired,
+  color: PropTypes.string,
 };
 
 export default Card;
