a8307371ff5eb1d808e2b3f9ea85cd259e79e14d
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-29 19:47:58 +0800

    ✨ Added contentful keys.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({});
+module.exports = withSass({
+  env: {
+    CONTENTFUL_SPACE: '8p3jtegjfjso',
+    CONTENTFUL_TOKEN: 'JoXnJAUGtmi0bY0P4cFc9DrXQIdZJqyW05hQz369p-s',
+  },
+});
