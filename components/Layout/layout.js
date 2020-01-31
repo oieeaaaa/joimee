6d23777feebe2f666327d29ba8d56821a371736a
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-31 18:49:37 +0800

    ✨ Added apollo and setup apollo in layout.

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import fetch from 'isomorphic-unfetch';
+import { ApolloProvider } from '@apollo/react-common';
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import Header from 'components/Header/header';
 import Footer from 'components/Footer/footer';
 import 'scss/main.scss';
 
+const client = new ApolloClient({
+  ssrMode: true,
+  link: createHttpLink({
+    uri: process.env.API,
+    credentials: 'same-origin',
+    fetch,
+  }),
+  cache: new InMemoryCache(),
+});
+
 const Layout = ({ title, children }) => (
-  <>
+  <ApolloProvider client={client}>
     <Head>
       <title>{title}</title>
     </Head>
     <Header />
     {children}
     <Footer />
-  </>
+  </ApolloProvider>
 );
 
 Layout.propTypes = {
--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({});
+module.exports = withSass({
+  env: {
+    API: 'https://api-apeast.graphcms.com/v1/ck60hoj4x274d01fl3ghx5a6m/master',
+  },
+});
