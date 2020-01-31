11dffbb9defad9cb3643e2dd51637d9a045bc273
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-31 18:48:50 +0800

    👌 Updated <Card />.

--- components/Card/card.js
+++ components/Card/card.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// to be removed after development.
-export const FAKE_PROJECTS = [
-  {
-    title: 'Co Hiking',
-    path: 'co-hiking',
-    imgLink: 'https://bit.ly/2NJbGXu',
-    color: '#D6E5DC',
-  },
-  {
-    title: 'Sentinental Software',
-    path: 'sentinental-software',
-    imgLink: 'https://bit.ly/2sJtFWJ',
-    color: '#358F6C',
-  },
-  {
-    title: 'Vespa',
-    path: 'vespa',
-    imgLink: 'https://cdn.dribbble.com/users/4189231/screenshots/9653758/media/634bcc1a23220d074f322749524f423e.jpg',
-    color: '#FACB05',
-  },
-];
-
 const Card = ({ title, imgLink, color }) => (
   <div className="card">
     <figure className="card__img-container">
