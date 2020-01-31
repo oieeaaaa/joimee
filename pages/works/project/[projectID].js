b4070ddc46f02a8baed268753c881085fd1ab45b
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-31 20:11:24 +0800

    🔥 Removed dead codes and stuff.

--- pages/works/project/[projectID].js
+++ pages/works/project/[projectID].js
@@ -1,53 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'next/router';
-import gql from 'graphql-tag';
-import client from 'js/client';
-import includeAsset from 'js/includeAsset';
 import Layout from 'components/Layout/layout';
 
-const query = gql`
-  query ProjectItem($slug: String!) {
-    projects(slug: $slug) @rest(type: "ProjectItem", path: "/entries?content_type=projects&fields.slug={args.slug}") {
-      sys,
-      total,
-      includes,
-      items @type(name: "ProjectItem") {
-        sys @type(name: "ProjectItem") {
-          id,
-          createdAt
-        },
-        fields
-      }
-    }
-  }
-`;
-
 const Project = ({ router }) => {
-  const [project, setProject] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
-    if (!router.query.projectID) return;
-    client.query(
-      {
-        query,
-        variables: {
-          slug: router.query.projectID,
-        },
-      },
-    ).then(({ data }) => {
-      const projects = includeAsset(data.projects);
-      setProject(projects.items[0]);
-      setIsLoading(false);
-    });
+    if (!router.query.slug) return;
+    console.log(router);
   }, [router]);
 
-  if (isLoading) return 'Loading...';
-
   return (
-    <Layout title={project.fields.title}>
-      <h1>{project.fields.title}</h1>
+    <Layout title="Project">
+      <h1>{router.query.projectID}</h1>
     </Layout>
   );
 };
@@ -57,6 +21,7 @@ Project.propTypes = {
     pathname: PropTypes.string.isRequired,
     query: PropTypes.shape({
       projectID: PropTypes.string,
+      slug: PropTypes.string,
     }).isRequired,
   }).isRequired,
 };
