c09e8a2ccd164cf4ddbce81fda6e32584671888d
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-01-31 23:01:12 +0800

    🏗  Wired up project view.

--- pages/works/project/[projectID].js
+++ pages/works/project/[projectID].js
@@ -1,18 +1,49 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import { withRouter } from 'next/router';
-import Layout from 'components/Layout/layout';
+import { withLayout } from 'components/Layout/layout';
+
+const GET_PROJECT = gql`
+query Project($id: ID) {
+  project(where: { id: $id }) {
+    id,
+    title,
+    createdAt,
+    content,
+    siteUrl,
+    image {
+      url
+    }
+  }
+}
+`;
 
 const Project = ({ router }) => {
+  const [loadProject, { data, loading }] = useLazyQuery(
+    GET_PROJECT,
+    {
+      variables: {
+        id: '',
+      },
+    },
+  );
+
   useEffect(() => {
-    if (!router.query.slug) return;
-    console.log(router);
+    const id = router.query.projectID;
+    if (!id) return;
+
+    loadProject({
+      variables: {
+        id,
+      },
+    });
   }, [router]);
 
+  if (loading || !data) return <p>Loading...</p>;
   return (
-    <Layout title="Project">
-      <h1>{router.query.projectID}</h1>
-    </Layout>
+    <h1>{data.project.title}</h1>
   );
 };
 
@@ -21,9 +52,8 @@ Project.propTypes = {
     pathname: PropTypes.string.isRequired,
     query: PropTypes.shape({
       projectID: PropTypes.string,
-      slug: PropTypes.string,
     }).isRequired,
   }).isRequired,
 };
 
-export default withRouter(Project);
+export default withLayout(withRouter(Project));
