e26d65d0816ef89c6830e566faa59bc04dd845e9
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-02-14 15:27:59 +0800

    🐛 Fixed: While loading the body is still scrollable.

--- components/Loading/loading.js
+++ components/Loading/loading.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Loading = () => (
-  <div className="loading" />
-);
+const Loading = () => {
+  useEffect(() => {
+    const app = document.body;
+
+    if (app) {
+      app.classList.add('hide');
+    }
+
+    return () => {
+      if (app.classList.contains('hide')) {
+        app.classList.remove('hide');
+      }
+    };
+  }, []);
+
+  return <div className="loading" />;
+};
 
 export default Loading;
