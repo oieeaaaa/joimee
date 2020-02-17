7ee3f323a1ffe6953a2a9469523931246ecfd389
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-02-17 12:09:24 +0800

    🔥 Updated app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ThemePicker from 'components/ThemePicker/themePicker';
+import fetch from 'isomorphic-unfetch';
+import { ApolloProvider } from '@apollo/react-common';
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import 'scss/main.scss';
+
+const client = new ApolloClient({
+  ssrMode: true,
+  link: createHttpLink({
+    uri: process.env.API,
+    credentials: 'same-origin',
+    fetch,
+  }),
+  cache: new InMemoryCache(),
+});
 
 const App = ({ Component, pageProps }) => (
-  <div className="app">
-    <Component {...pageProps} />
-    <ThemePicker />
-  </div>
+  <ApolloProvider client={client}>
+    <div className="app">
+      <Component {...pageProps} />
+      <ThemePicker />
+    </div>
+  </ApolloProvider>
 );
 
 App.propTypes = {
