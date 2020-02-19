6acbf255c98c3cfa59966a21ab94c91d8b6c8ced
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-02-19 18:15:56 +0800

    👍 Used sw and manifest.

--- TASK.md
+++ TASK.md
@@ -10,6 +10,7 @@
 + Add Theming
 - Loading Effect when submitting a comment
 - Dark Mode when OSX is on dark mode
+- Support native ios
 
 # Bug Fix
 + Fix header not working on safari ios 12 device
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import PropTypes from 'prop-types';
 import ThemePicker from 'components/ThemePicker/themePicker';
 import fetch from 'isomorphic-unfetch';
@@ -18,14 +19,49 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const App = ({ Component, pageProps }) => (
-  <ApolloProvider client={client}>
-    <div className="app">
-      <Component {...pageProps} />
-      <ThemePicker />
-    </div>
-  </ApolloProvider>
-);
+
+class App extends React.Component {
+  componentDidMount() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js');
+    }
+  }
+
+  componentWillUnmount() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister();
+      });
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <ApolloProvider client={client}>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/icon-72x72.png" />
+          <link rel="apple-touch-icon" href="/icons/icon-96x96.png" />
+          <link rel="apple-touch-icon" href="/icons/icon-128x128.png" />
+          <link rel="apple-touch-icon" href="/icons/icon-144x144.png" />
+          <link rel="apple-touch-icon" href="/icons/icon-152x152.png" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+          <link rel="apple-touch-icon" href="/icons/icon-384x384.png" />
+          <link rel="apple-touch-icon" href="/icons/icon-512x512.png" />
+          <link rel="apple-touch-startup-image" href="/images/launch-screen.png" />
+
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+        </Head>
+        <div className="app">
+          <Component {...pageProps} />
+          <ThemePicker />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
 
 App.propTypes = {
   Component: PropTypes.func.isRequired,
