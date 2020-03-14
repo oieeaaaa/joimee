f0908396a6579ef0701003bb980ebc462f024eb5
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-03-14 15:59:40 +0800

    ✨ Added: mdx support in config.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-const withSass = require('@zeit/next-sass');
-
-module.exports = withSass({
-  env: {
-    API: 'https://api-apeast.graphcms.com/v1/ck60hoj4x274d01fl3ghx5a6m/master',
-  },
+const sass = require('@zeit/next-sass');
+const mdx = require("@next/mdx")({
+  extension: /\.mdx?$/,
 });
+const withPlugins = require('next-compose-plugins');
+
+module.exports = withPlugins([
+  [sass],
+  [mdx, {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  }],
+]);
