32f9cea7da1d1c039ad2171908995c6cd99a7e0d
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-03-14 20:08:29 +0800

    🔥 Updated: recentProjects.

--- components/RecentProjects/recentProjects.js
+++ components/RecentProjects/recentProjects.js
@@ -1,48 +1,27 @@
 import React from 'react';
 import Heading from 'components/Heading/heading';
 import Link from 'next/link';
-import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
 import Card from 'components/Card/card';
 import Loading from 'components/Loading/loading';
-
-const GET_RECENT_PROJECTS = gql`
-query {
-  projects(orderBy: createdAt_ASC, last: 3) {
-    id,
-    title,
-    createdAt,
-    theme {
-      hex
-    },
-    image {
-      url
-    },
-  }
-}
-`;
+import projects from 'data/projects';
 
 const RecentProjects = () => {
-  const { loading, error, data } = useQuery(GET_RECENT_PROJECTS);
-
-  if (loading) return <Loading />;
-  if (error) return <div>Error!!!</div>;
+  console.log(projects);
 
   return (
     <section className="recent-projects">
       <div className="grid">
         <Heading text="Recent Projects" />
-        {data.projects.map(project => (
+        {projects.map(project => (
           <Link
-            key={project.id}
-            href="/works/project/[projectID]"
-            as={`/works/project/${project.id}`}
+            key={project.path}
+            href={project.path}
           >
             <a className="recent-projects__item">
               <Card
                 title={project.title}
-                imgLink={project.image.url}
-                color={project.theme.hex}
+                imgLink={project.image}
+                color={project.color}
               />
             </a>
           </Link>
