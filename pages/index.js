ed7cbf6580501ae56f338a018f3f3fb4431af1ec
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-03-14 20:09:08 +0800

    ✨ Equipped: Recent projects component.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from 'components/Layout/layout';
 import Hero from 'components/Hero/hero';
-// import RecentProjects from 'components/RecentProjects/recentProjects';
+import RecentProjects from 'components/RecentProjects/recentProjects';
 import RecentPosts from 'components/RecentPosts/recentPosts';
 
 const Home = () => (
   <Layout title="Home" description="Landing Page">
     <Hero />
+    <RecentProjects />
     <RecentPosts />
   </Layout>
 );
