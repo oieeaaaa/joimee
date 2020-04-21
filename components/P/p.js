8a160c41ce7b9ac3dba1ba57e67d95bfe1041ca4
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-04-21 10:10:54 +0800

    ✨ Increased image width

--- components/P/p.js
+++ components/P/p.js
@@ -1,7 +1,18 @@
-const Paragraph = ({ children }) => (
-  <p className="p">
-    {children}
-  </p>
-);
+import { useEffect } from 'react';
 
+const Paragraph = ({ children }) => {
+  useEffect(() => {
+    const figure = document.querySelector('.p > figure');
+
+    if (figure) {
+      figure.parentNode.classList.add('image-wrapper');
+    }
+  }, []);
+
+  return (
+    <p className="p">
+      {children}
+    </p>
+  );
+}
 export default Paragraph;
