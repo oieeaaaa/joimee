d2db7746bd04dd2a2b0876d002fedd712790d3f2
Joimee Tan Cajandab <joimee.cajandab@codeantheory.com>
2020-12-21 21:58:39 +0800

    🏗  Refactored hero's texts

--- components/Hero/hero.js
+++ components/Hero/hero.js
@@ -5,26 +5,25 @@ import Text3D from 'components/Text3D/text3D';
 const Hero = () => (
   <section className="hero">
     <div className="grid">
-      <h1 className="hero__title">
-        <Link href="/works">
-          <a>
-            <Text3D aria-hidden className="hero__title-text" text="BOK'S" />
-            <Text3D aria-hidden className="hero__title-text" text="SITE" />
-          </a>
-        </Link>
-      </h1>
       <nav className="hero__nav">
         <Link href="/works">
           <a className="hero__nav-item">
-            Works
+            <Text3D text="Works" />
           </a>
         </Link>
         <Link href="/about">
           <a className="hero__nav-item">
-            About
+            <Text3D text="About" />
           </a>
         </Link>
       </nav>
+      <h1 className="hero__title">
+        <Link href="/works">
+          <a>
+            <Text3D aria-hidden className="hero__title-text" text="BOK'S SITE" />
+          </a>
+        </Link>
+      </h1>
     </div>
   </section>
 );
